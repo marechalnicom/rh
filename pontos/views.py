52f627d6f7b06f244251588ada427ef7ce761d50
nicom <marechalnicom@gmail.com>
2022-07-09 08:03:24 -0300

    ajutes para exibição de empresa

--- pontos/views.py
+++ pontos/views.py
@@ -7,8 +7,14 @@ from django.utils import timezone
 from .models import *
 
 class IndexView(generic.ListView):
-    template_name = 'rh/index.html'
-    context_object_name = 'latest_question_list'
+    template_name = 'pontos/index.html'
+    context_object_name = 'lista_empresas'
+
+    def get_queryset(self):
+        return Empresa.objects.all()
+class RegistrosView(generic.ListView):
+    template_name = 'pontos/registros.html'
+    context_object_name = 'lista_ultimos_pontos'
 
     def get_queryset(self):
         return RegistroPontos.objects.filter(
