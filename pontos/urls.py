061433611a0c1457cfe4a99dc7c8bb2e15162eab
nicom <marechalnicom@gmail.com>
2022-07-14 10:17:24 -0300

    adicionando funcoes p busca js

--- pontos/urls.py
+++ pontos/urls.py
@@ -2,6 +2,9 @@ from django.urls import  path
 
 from . import views
 
+app_name ='pontos'
 urlpatterns = [
     path("",  views.IndexView.as_view() , name='index'),
+    path("registros", views.registro, name="registros"),
+    path("empresas", views.empresa, name="empresas"),
 ]
--- pontos/views.py
+++ pontos/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponseRedirect
+from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import *
+from .models import Empresa, Funcionario, Usuario, RegistroPontos
 
 class IndexView(generic.ListView):
     template_name = 'pontos/index.html'
@@ -12,11 +12,29 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Empresa.objects.all()
-class RegistrosView(generic.ListView):
-    template_name = 'pontos/registros.html'
-    context_object_name = 'lista_ultimos_pontos'
+#class RegistrosView(generic.ListView):
+#    context_object_name = 'lista_ultimos_pontos'
+#
+    #def get_queryset(self):
+    #    return RegistroPontos.objects.filter(
+    #        ponto_data__lte=timezone.now()
+    #    ).order_by('-ponto_data')[:5]
+def empresa(request):
+    empresas = Empresa.objects.all()
+    resposta = {}
+    respost = []
+    for empresa in empresas:
+        respost.append({str(empresa):str(empresa.cnpj)})
+        print (respost,type(respost))
+    resposta = {"empresa":respost}
+    return JsonResponse(resposta)
 
-    def get_queryset(self):
-        return RegistroPontos.objects.filter(
-            ponto_data__lte=timezone.now()
-        ).order_by('-ponto_data')[:5]
+def registro(request):
+    registros = RegistroPontos.objects.filter(ponto_data__lte=timezone.now()).order_by('-ponto_data')[:15]
+    resposta = {}
+    respost = []
+    for regis in registros:
+        respost.append({str(regis.ponto_data):str(regis.funcionario)})
+    resposta = {"registro":respost}
+
+    return JsonResponse(resposta)
